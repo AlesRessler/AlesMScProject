Fixed color scaling in plotting of the continuous spherical function

visualization/spherical_functions_visualisation.py
@@ -3,7 +3,7 @@ import numpy as np
 from sphericalharmonics.utils import get_storage_index
 from sphericalharmonics.basis_functions import real_and_antipodal_spherical_harmonic_basis
 
-def get_spherical_function_values_from_spherical_expansion(expansion_coefficients, max_degree, resolution=50):
+def get_spherical_function_values_from_spherical_expansion(expansion_coefficients, max_degree, resolution=50, min_value=None, max_value=None):
     """
     Computes values of spherical function that is passed as an argument at a specified resolution. These values can be used for plotting.
     
@@ -11,6 +11,8 @@ def get_spherical_function_values_from_spherical_expansion(expansion_coefficient
     expansion_coefficients (np.array): array of spherical harmonics expansion coefficients representing the function to compute the values of
     max_degree (int): maximum degree of spherical harmonics used for the expansion
     resolution (int): specifies resolution of the visualization
+    min_value (int): minimum value of the function used for normalization to colormap scale (if None is given the minimum is calculated from the sample points)
+    max_value (int): maximum value of the function used for normalization to colormap scale (if None is given the maximum is calculated from the sample points)
     
     Returns:
     (np.array,np.array,np.array,np.array) The first three arrays correspond to the x,y,z coordinates on the sphere and the last array contains the function values (the function values are scaled to range [0,1]).
@@ -30,9 +32,23 @@ def get_spherical_function_values_from_spherical_expansion(expansion_coefficient
     for l in range(0, max_degree + 1, 2):
         for m in range(-l, l + 1):
             coefficient_index = get_storage_index(l,m)
+            
             fcolors += real_and_antipodal_spherical_harmonic_basis(l, m, thetas, phis) * expansion_coefficients[coefficient_index]
     
-    fmax, fmin = fcolors.max(), fcolors.min()
+    # Normalize to range [0,1]
+    fmin = None
+    fmax = None
+    
+    if(min_value is None):
+        fmin = fcolors.min()
+    else:
+        fmin = min_value
+    
+    if(max_value is None):
+        fmax = fcolors.max()
+    else:
+        fmax = max_value
+    
     fcolors = (fcolors - fmin)/(fmax - fmin)
     fcolors = np.nan_to_num(fcolors,nan=0.0)
     