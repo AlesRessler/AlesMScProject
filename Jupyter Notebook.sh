27a8a6c5be82fda1955c35abe7d8625f022f281e
Aleš Ressler <alesressler@Aless-MacBook-Pro.local>
2023-08-01 13:19:05 +0200

    Added Jupyter Notebook start script

---
+++ Jupyter Notebook.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+cd /Users/alesressler/Documents/MSc-Project/AlesMScProject
+
+source /Users/alesressler/miniconda3/etc/profile.d/conda.sh
+
+conda activate msc-project
+
+jupyter notebook
