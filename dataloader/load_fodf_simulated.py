74042f489c4b35020ca9c73f2eea4ffca32cd5f7
Aleš Ressler <alesressler@Aless-MacBook-Pro.local>
2023-08-14 11:32:59 +0200

    Implemented fibre fractions in fODF simulation

--- dataloader/load_fodf_simulated.py
+++ dataloader/load_fodf_simulated.py
@@ -5,7 +5,7 @@ from sphericalharmonics.utils import get_storage_index, get_number_of_coefficien
 from preprocessing.data_transformations import convert_coords_from_cartesian_to_spherical
 
 
-def load_fodf_simulated(max_degree, fibre_orientations=None):
+def load_fodf_simulated(max_degree, fibre_orientations=None, fibre_fractions=None):
     """
     Computes spherical harmonics expansion coefficients of fODF with given fibre orientations
 
@@ -19,6 +19,8 @@ def load_fodf_simulated(max_degree, fibre_orientations=None):
 
     if (fibre_orientations is None):
         fibre_orientations = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    if (fibre_fractions is None):
+        fibre_fractions = [0.333, 0.333, 0.333]
 
     thetas, phis = convert_coords_from_cartesian_to_spherical(fibre_orientations.T)
 
@@ -26,12 +28,15 @@ def load_fodf_simulated(max_degree, fibre_orientations=None):
     spherical_harmonics_coefficients = np.zeros(number_of_coefficients)
 
     for i in range(len(fibre_orientations)):
+        single_population_fodf = np.zeros(number_of_coefficients)
+
         for l in range(0, max_degree + 1, 2):
             for m in range(-l, l + 1):
-                spherical_harmonics_coefficients[
-                    get_storage_index(l, m)] += real_and_antipodal_spherical_harmonic_basis(l,
-                                                                                            m,
-                                                                                            thetas[
-                                                                                                i],
-                                                                                            phis[i])
+                storage_index = get_storage_index(l, m)
+                single_population_fodf[storage_index] += real_and_antipodal_spherical_harmonic_basis(l,
+                                                                                                     m,
+                                                                                                     thetas[
+                                                                                                         i],
+                                                                                                     phis[i])
+        spherical_harmonics_coefficients += single_population_fodf * fibre_fractions[i]
     return spherical_harmonics_coefficients
