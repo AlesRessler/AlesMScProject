7002fd51bd60d8849ff6aab5430b0da2612a1674
Aleš Ressler <alesressler@Aless-MacBook-Pro.local>
2023-08-20 23:13:43 +0200

    Added multiple dataset generation

--- visualization/spherical_functions_visualisation.py
+++ visualization/spherical_functions_visualisation.py
@@ -3,7 +3,9 @@ import numpy as np
 from sphericalharmonics.utils import get_storage_index
 from sphericalharmonics.basis_functions import real_and_antipodal_spherical_harmonic_basis
 
-def get_spherical_function_values_from_spherical_expansion(expansion_coefficients, max_degree, resolution=50, min_value=None, max_value=None):
+
+def get_spherical_function_values_from_spherical_expansion(expansion_coefficients, max_degree, resolution=50,
+                                                           min_value=None, max_value=None, normalize=True):
     """
     Computes values of spherical function that is passed as an argument at a specified resolution. These values can be used for plotting.
     
@@ -17,39 +19,41 @@ def get_spherical_function_values_from_spherical_expansion(expansion_coefficient
     Returns:
     (np.array,np.array,np.array,np.array) The first three arrays correspond to the x,y,z coordinates on the sphere and the last array contains the function values (the function values are scaled to range [0,1]).
     """
-    
+
     thetas = np.linspace(0, np.pi, resolution)
-    phis = np.linspace(0, 2*np.pi, resolution)
+    phis = np.linspace(0, 2 * np.pi, resolution)
     thetas, phis = np.meshgrid(thetas, phis)
 
     # The Cartesian coordinates of the unit sphere
     x = np.sin(thetas) * np.cos(phis)
     y = np.sin(thetas) * np.sin(phis)
     z = np.cos(thetas)
-    
+
     fcolors = np.zeros((resolution, resolution))
-    
+
     for l in range(0, max_degree + 1, 2):
         for m in range(-l, l + 1):
-            coefficient_index = get_storage_index(l,m)
-            
-            fcolors += real_and_antipodal_spherical_harmonic_basis(l, m, thetas, phis) * expansion_coefficients[coefficient_index]
-    
+            coefficient_index = get_storage_index(l, m)
+
+            fcolors += real_and_antipodal_spherical_harmonic_basis(l, m, thetas, phis) * expansion_coefficients[
+                coefficient_index]
+
     # Normalize to range [0,1]
     fmin = None
     fmax = None
-    
-    if(min_value is None):
+
+    if (min_value is None):
         fmin = fcolors.min()
     else:
         fmin = min_value
-    
-    if(max_value is None):
+
+    if (max_value is None):
         fmax = fcolors.max()
     else:
         fmax = max_value
-    
-    fcolors = (fcolors - fmin)/(fmax - fmin)
-    fcolors = np.nan_to_num(fcolors,nan=0.0)
-    
-    return(x, y, z, fcolors)
+
+    if(normalize):
+        fcolors = (fcolors - fmin) / (fmax - fmin)
+        fcolors = np.nan_to_num(fcolors, nan=0.0)
+
+    return (x, y, z, fcolors)
