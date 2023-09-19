f240c1662a812455c396828f4e3d0cd7d2e6d61b
Aleš Ressler <alesressler@Aless-MacBook-Pro.local>
2023-09-19 19:54:48 +0200

    Finished U-Net evaluation

--- models/neuralnetwork/spherical_cnn.py
+++ models/neuralnetwork/spherical_cnn.py
@@ -36,6 +36,67 @@ class SphericalConvolution(torch.nn.Module):
         return spherical_convolution
 
 
+class UNet(torch.nn.Module):
+    def __init__(self, number_of_shells, output_size, max_degree, spherical_fourier_transform,
+                 inverse_spherical_fourier_transform):
+        super().__init__()
+
+        number_of_sh_coefficients = get_number_of_coefficients(max_degree)
+
+        self.encoder1 = SphericalConvolution(max_degree, number_of_shells, 16, number_of_sh_coefficients=number_of_sh_coefficients)
+        self.encoder2 = SphericalConvolution(max_degree, 16, 32, number_of_sh_coefficients=number_of_sh_coefficients)
+        self.encoder3 = SphericalConvolution(max_degree, 32, 64, number_of_sh_coefficients=number_of_sh_coefficients)
+        self.encoder4 = SphericalConvolution(max_degree, 64, 128, number_of_sh_coefficients=number_of_sh_coefficients)
+
+        self.decoder1 = SphericalConvolution(max_degree, 128, 64, number_of_sh_coefficients=number_of_sh_coefficients)
+        self.decoder2 = SphericalConvolution(max_degree, 128, 32, number_of_sh_coefficients=number_of_sh_coefficients)
+        self.decoder3 = SphericalConvolution(max_degree, 64, 16, number_of_sh_coefficients=number_of_sh_coefficients)
+        self.decoder4 = SphericalConvolution(max_degree, 32, 16, number_of_sh_coefficients=number_of_sh_coefficients)
+        self.decoder5 = SphericalConvolution(max_degree, 16, output_size, number_of_sh_coefficients=number_of_sh_coefficients)
+
+        self.register_buffer("sft", spherical_fourier_transform)
+        self.register_buffer("isft", inverse_spherical_fourier_transform)
+
+    def nonlinearity(self, x):
+        return (
+                self.sft @ torch.nn.functional.relu(self.isft @ x.unsqueeze(-1))
+        ).squeeze(-1)
+
+    def forward(self, x):
+        # Encoder
+        encoded1 = self.encoder1(x)
+        encoded1 = self.nonlinearity(encoded1)
+
+        encoded2 = self.encoder2(encoded1)
+        encoded2 = self.nonlinearity(encoded2)
+
+        encoded3 = self.encoder3(encoded2)
+        encoded3 = self.nonlinearity(encoded3)
+
+        encoded4 = self.encoder4(encoded3)
+        encoded4 = self.nonlinearity(encoded4)
+
+        # Decoder
+        decoded1 = self.decoder1(encoded4)
+        decoded1 = torch.cat([decoded1, encoded3], dim=1)
+        decoded1 = self.nonlinearity(decoded1)
+
+        decoded2 = self.decoder2(decoded1)
+        decoded2 = torch.cat([decoded2, encoded2], dim=1)
+        decoded2 = self.nonlinearity(decoded2)
+
+        decoded3 = self.decoder3(decoded2)
+        decoded3 = torch.cat([decoded3, encoded1], dim=1)
+        decoded3 = self.nonlinearity(decoded3)
+
+        decoded4 = self.decoder4(decoded3)
+        decoded4 = self.nonlinearity(decoded4)
+
+        decoded5 = self.decoder5(decoded4)
+        decoded5 = torch.squeeze(decoded5)
+        return decoded5
+
+
 class SCNNModel(torch.nn.Module):
     def __init__(self, number_of_shells, output_size, max_degree, spherical_fourier_transform,
                  inverse_spherical_fourier_transform):
